a5a341304d9ef8d06ca80dbdba9150e4ad57d219
Alexander Nestorov <alexandernst@gmail.com>
2018-10-13 12:57:14 +0200

    Log errors

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 from decouple import config
 from cdmon_automator import CDMON
 
@@ -13,11 +14,10 @@ try:
     cdmon.create_record("TXT", {
         "redirect_type": "custom",
         "subdomain": "_acme-challenge",
-        "value": config("CERTBOT_VALIDATION")
+        "value": config("CERTBOT_VALIDATION", "Make sure to put a value here")
     })
-
-    cdmon.delete_record("TXT", "_acme-challenge")
 
     cdmon.terminate()
 except Exception as ex:
     cdmon.terminate()
+    sys.exit(ex)
